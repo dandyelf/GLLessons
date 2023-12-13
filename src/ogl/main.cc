4647f2f6aa93be2c8ca73381c4c4636ec1dc9331
leftrana <leftrana@student.21-school.ru>
2023-12-13 15:34:10 +0300

    amin fix

--- src/ogl/main.cc
+++ src/ogl/main.cc
@@ -7,13 +7,11 @@
 int main(int argc, char *argv[])
 {
     s21::Controller * controller_obj = s21::Controller::getInstance();
-    s21::Facade * facade_model_obj = new s21::Facade;
-    controller_obj->SetModel(facade_model_obj);
+    s21::Facade  facade_model_obj;
+    controller_obj->SetModel(&facade_model_obj);
     QApplication a(argc, argv);
     MainWindow w;
     w.SetControllerObj(controller_obj);
     w.show();
-//    if(facade_model_obj != nullptr) delete facade_model_obj;
-    qDebug() << "File OK" << "Here" << QDate::currentDate();
     return a.exec();
 }
