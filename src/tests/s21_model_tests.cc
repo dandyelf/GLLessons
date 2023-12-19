bbbac71ceabde7ee90a0bc3f815abea6804d61de
leftrana <leftrana@student.21-school.ru>
2023-12-19 19:30:48 +0300

    obj add

--- src/tests/s21_model_tests.cc
+++ src/tests/s21_model_tests.cc
@@ -13,6 +13,6 @@ TEST(ModelFacade, GetObj) {
 
 TEST(ModelFacade, StartOpen) {
   s21::Facade a;
-  a.OpenObj(obj_path + "/mustang.obj");
-  EXPECT_EQ(a.GetObject()->count_of_vertexes, 753673);
+  a.OpenObj(obj_path + "/tetrahedron.obj");
+  EXPECT_EQ(a.GetObject()->count_of_vertexes, 38);
 }
